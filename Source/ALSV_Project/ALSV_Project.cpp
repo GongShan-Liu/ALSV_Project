afc3f30ec6ab54fe1ac35f1c36119fd724800b61
GongShan-Liu <gong405884210@gmail.com>
2026-08-20 20:56:15 +0800

    更新

--- Source/ALSV_Project/ALSV_Project.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ALSV_Project.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ALSV_Project, "ALSV_Project" );
-
-DEFINE_LOG_CATEGORY(LogALSV_Project)
--- Source/ALSV_Project/ALSV_ProjectGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ALSV_ProjectGameMode.h"
-
-AALSV_ProjectGameMode::AALSV_ProjectGameMode()
-{
-	// stub
-}
